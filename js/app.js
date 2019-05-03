355c99e86178b34b81d46e317c498bdd44ac528f
nycole <spammy.seibert2@gmail.com>
2019-05-03 14:39:25 -0700

    message

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// question 1
+
 var username = prompt('Welcome to the page, what\'s your name?');
 alert('Hello ' + username + ' wanna play a game? Well, we\'re playing it anyway.');
 console.log('the users name is ' + username);
 
-//question 2
-
+//question 1
+function questionOne (){
 var answer = prompt('I enjoy the beach, yes or no?');
 answer = answer.toLowerCase();
 if (answer == 'yes') {
@@ -15,8 +15,11 @@ if (answer == 'yes') {
     alert('Congratulations, the ocean is too salty for my hair.');
 }
 console.log('user answered ' + answer);
+}
+questionOne();
 
-// question 3
+// question 2
+function questionTwo (){
 var two = prompt('Do you think I hate cats?');
 two = two.toLowerCase();
 if (two == 'no') {
@@ -25,8 +28,11 @@ if (two == 'no') {
     alert('Congratulations, I pretty much despise them.');
 }
 console.log('user answered ' + two);
+}
+questionTwo();
 
-// question 4
+// question 3
+function questionThree (){
 var three = prompt('my favorite holliday is Valentines day, yes or no?');
 three = three.toLowerCase();
 if (three == 'yes') {
@@ -35,8 +41,11 @@ if (three == 'yes') {
     alert('Way to go! Halloween is actually my absalute favorite!.');
 }
 console.log('user answered ' + three);
+}
+questionThree();
 
-// question 5
+// question 4
+function questionFour (){
 var four = prompt('Would you agree I\'m a very social person and love being around people?');
 four = four.toLowerCase();
 if (four == 'no') {
@@ -45,8 +54,11 @@ if (four == 'no') {
     alert('correct! I love people and can\'t live without social interaction.');
 }
 console.log('user answered ' + four);
+}
+questionFour();
 
-// question 6
+// question 5
+function questionFive (){
 var five = prompt('Lastly, do you believe I have a clean bill of health?');
 five = five.toLowerCase();
 if (five == 'yes') {
@@ -55,8 +67,28 @@ if (five == 'yes') {
     alert('That\'s right sadly. no one actually has perfect health wether it be physically or mentally.');
 }
 console.log('user answered ' + five);
-
-
-function fruit(mango){
-
 }
+questionFive();
+
+// question 6
+function questionSix (){
+    var attemptsRemaining = 4;
+    while(attemptsRemaining > 0 && answerSix !== 2){
+        var answerSix = prompt( 'What do you think is my favorite number?');
+        attemptsRemaining--;
+        if(answerSix === 2){
+            alert('Congradulations, that is correct');
+            console.log('user guessed ' + answerSix + ' correctly');
+        } else if(answerSix > 2){
+            alert(answerSix + ' is incorect. You guessed too high');
+            console.log('user guessed ' + answereSix + ' incorrectly')
+        } else if(answerSix < 2){
+            alert(answerSix + ' is incorrect. You guessed too low.');
+            console.log('user guessed ' + answereSix + ' incorrectly')
+        } 
+        if(attemptsRemaining === 0){
+            alert('You are out of guesses');
+        }
+    }
+} 
+questionSix();
